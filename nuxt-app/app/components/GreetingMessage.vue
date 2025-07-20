3593a5b46c8ea052c18f51b237484857defa9d97
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-07-20 23:10:32 +0900

    add: defineProps

---
+++ nuxt-app/app/components/GreetingMessage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{ greeting }} {{ name }}さん</p>
+    <p v-if="message">{{ message }} </p>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    greeting: {
+      type: String,
+      required: true,
+      default: 'こんにちは'
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  });
+</script>
+
+<style scoped>
+p {
+  color: #333;
+  font-size: 1.1em;
+}
+</style>
---
+++ nuxt-app/app/pages/greeting.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Prop Sample</h1>
+    <GreetingMessage greeting="Hello" name="World" />
+    <GreetingMessage :greeting="dynamicGreeting" :name="userName" :message="optionalMessage" />
+    <GreetingMessage greeting="こんにちは" name="ユーザー" />
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const dynamicGreeting = ref('おはよう');
+const userName = ref('太郎');
+const optionalMessage = ref('これはオプションのメッセージです。');
+</script>
