00a758ad2c32c48d2f32420c37dfaa75cde59cab
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-07-21 19:26:32 +0900

    add: create user information page to display user IDs

---
+++ nuxt-app/app/pages/users/[...id].vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>ユーザ情報</h1>
+        <p>ユーザIDs</p>
+        <ul>
+            <li v-for="id in route.params.id" :key="id">
+                {{ id }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const route = useRoute();
+
+</script>
