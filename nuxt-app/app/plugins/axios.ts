a8cca99eafb45adac6185acf5d70e57a600c1e64
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-07-25 23:50:29 +0900

    refactor: comment out axios plugin implementation

--- nuxt-app/app/plugins/axios.ts
+++ nuxt-app/app/plugins/axios.ts
@@ -1,43 +1,43 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const api = axios.create({
-    baseURL: nuxtApp.$config.apiBaseUrl,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+// export default defineNuxtPlugin((nuxtApp) => {
+//   const api = axios.create({
+//     baseURL: nuxtApp.$config.apiBaseUrl,
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
 
-  // Request interceptor
-  api.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem('authToken');
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    (error) => {
-      console.error('Request error:', error);
-      return Promise.reject(error);
-    }
-  );
+//   // Request interceptor
+//   api.interceptors.request.use(
+//     (config) => {
+//       const token = localStorage.getItem('authToken');
+//       if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//       }
+//       return config;
+//     },
+//     (error) => {
+//       console.error('Request error:', error);
+//       return Promise.reject(error);
+//     }
+//   );
 
-  // Response interceptor
-  api.interceptors.response.use(
-    (response) => {
-      // Handle successful responses
-      return response;
-    },
-    (error) => {
-      console.error('Response error:', error);
-      return Promise.reject(error);
-    }
-  );
+//   // Response interceptor
+//   api.interceptors.response.use(
+//     (response) => {
+//       // Handle successful responses
+//       return response;
+//     },
+//     (error) => {
+//       console.error('Response error:', error);
+//       return Promise.reject(error);
+//     }
+//   );
 
-  return {
-    provide: {
-      api,
-    },
-  };
-});
+//   return {
+//     provide: {
+//       api,
+//     },
+//   };
+// });
