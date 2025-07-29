87d8a6da44f0547e05c08ccc144d1937d868b5cc
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-07-29 23:59:14 +0900

    add: implement logout component with logout functionality

---
+++ nuxt-app/app/components/logout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button @click="handleLogout">
+            ログアウト
+        </button>
+    </div>
+</template>
+
+<script setup>
+const authStore = useAuthStore();
+
+const handleLogout = () => {
+    authStore.logout();
+    console.log('Logged out successfully');
+    navigateTo('/login');
+}
+</script>
